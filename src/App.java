8fb23759a2811fd7b21c190d5e3da5c8c86ce849
KKDN7355 <KevinKDNguyen@gmail.com>
2025-01-20 16:57:25 +1100

    Gravity inversion!

--- src/App.java
+++ src/App.java
@@ -2,14 +2,13 @@ import javax.swing.*;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        // game board dimensions
         int boardWidth = 360;
         int boardHeight = 640;
 
         JFrame frame = new JFrame("Flappy Bird");
         frame.setSize(boardWidth, boardHeight);
-        frame.setLocationRelativeTo(null);                      // sets game board to center of screen
-        frame.setResizable(false);                      // prevents user or anything from resizing the game board
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         FlappyBird flappyBird = new FlappyBird();
